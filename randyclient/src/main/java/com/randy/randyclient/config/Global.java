41382b6f024cde689d29bf38b97978e8fbe828f4
Randy <rainmonth@yeah.net>
2017-04-13 19:32:15 +0800

    add global config file

---
+++ randyclient/src/main/java/com/randy/randyclient/config/Global.java
@@ -0,0 +1,12 @@
+package com.randy.randyclient.config;
+
+import java.util.Locale;
+
+/**
+ * Created by RandyZhang on 2017/4/13.
+ */
+
+public class Global {
+    public static Locale LOCALE = Locale.CHINA;
+    public static String TAG = "RandyClient";
+}
